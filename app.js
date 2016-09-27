54d8772a521c772301f49eb5b09eba32f83e2528
Troy <lambe1tm@gmail.com>
2016-09-27 13:23:24 -0400

    Added Event Listener without using onclick in html

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-test = new Date()
-month = test.getMonth()
-month = (month * 1) + 1
-day = test.getDate()
-year = test.getFullYear()
-document.write(" ",month,"/",day,"/",year," ")
+function myClickFunction(){
+	var x=document.getElementById("main-menu");
 
+	if (x.className==="display-none") {
+		x.className="display-block";
+	} else{
+		x.className="display-none";
+	}
 
-function myClickFunction() {
-  	var x=document.getElementById("main-menu");
-  
- 	if (x.style.display="none"){
- 		x.style.display="block";
-  	
-  	} else (x.style.display="block"){
- 		x.style.display="none";
- 	}
 }
 
+
+var el = document.getElementById("hamburger");
+el.addEventListener("click", myClickFunction, false);
